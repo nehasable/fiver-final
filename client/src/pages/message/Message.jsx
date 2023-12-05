aa91803af801e569abbde02856069b57573167a7
pedophilips <nehasable45@gmail.com>
2023-12-06 00:57:27 +0530

    commit

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,11 +1,110 @@
 
 
+import { Link } from "react-router-dom";
+import "./Message.css";
+
 const Message = () => {
   return (
-    <div>
-      message
+    <div className="message">
+      <div className="message-container">
+        <span className="breadcrumbs">
+          <Link to="/messages" className="link">Messages</Link> -John Doe 
+        </span>
+        <div className="new-messages">
+          <div className="message-item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item-owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="message-item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item-owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="message-item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          <div className="item-owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+          
+          <div className="message-item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iure
+              mollitia perspiciatis officiis voluptate? Sequi quae officia
+              possimus, iusto labore alias mollitia eveniet nemo placeat
+              laboriosam nisi animi! Error, tenetur!
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="write-message">
+          <textarea type="text" placeholder="write a message" />
+          <button>Send</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
--- client/src/pages/mygigs/Mygigs.jsx
+++ client/src/pages/mygigs/Mygigs.jsx
@@ -1,11 +1,75 @@
-
+import { Link } from "react-router-dom"
+import "./Mygigs.css"
 
 const Mygigs = () => {
+  const currentUser={
+    id: 1,
+    username: "Anna",
+    isSeller: false,
+  }
   return (
     <div className="mygigs">
-      <h1>Gigs</h1>
-      <button className="price-btn">Add new Gig</button>
+    <div className="mygigs-container">
+    <div className="mygigs-title">
+      <h1>{currentUser.isSeller ? "Gigs" :"Orders"}</h1>
+      {currentUser.isSeller && (
+      <Link to="/add" className="link"> <button className="mygigs-button">Add New Gig</button></Link>
+      )}</div>
       
+      <table>
+        <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+        </tr>
+      
+        <tr>
+          <td> <img
+                className="mygigs-image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              /></td>
+              <td>AI Art</td>
+              <td>59.<sup>99</sup></td>
+              <td>20</td>
+              <td> <img  className="mygigs-delete" src="./img/delete.png"></img></td>
+        </tr>
+
+        <tr>
+        <td>
+              <img
+                className="mygigs-image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>120.<sup>99</sup></td>
+            <td>41</td>
+            <td>
+              <img className="mygigs-delete" src="./img/delete.png" alt="" />
+            </td>
+        </tr>
+        <tr>
+        <td>
+              <img
+                className="mygigs-image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            
+            <td>High quality digital character</td>
+            <td>79.<sup>99</sup></td>
+            <td>55</td>
+            <td>
+              <img className="mygigs-delete" src="./img/delete.png" alt="" />
+            </td>
+          </tr>
+      </table>
+      </div>
     </div>
   )
 }
