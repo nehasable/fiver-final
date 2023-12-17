0c83616db073d24db34463e295b3069edc8f15a7
nehasable <nehasable45@gmail.com>
2023-12-17 15:23:27 +0530

    commit

---
+++ server/controller/user_controller.js
@@ -0,0 +1,4 @@
+//contains all the functions that will be implemented through routes
+export const deleteUser=(req,resp)=>{
+
+}
--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
+const UserRouter = require("./routes/user_route.js");
 const express=require("express")
 const mongoose=require("mongoose")
 const app=express()
 
 const url = 'mongodb://localhost:27017/fiver';
 
-mongoose.connect(url, {
-
-    useUnifiedTopology: true,
-  });
+mongoose.connect(url);
   
   const db = mongoose.connection;
 
   db.on('error', (error) => {
     console.error('MongoDB connection error:', error);
   });
-  
+app.use("/api/users",UserRouter)  
+
 app.listen("8000",()=>{
     console.log("connected")
 })
