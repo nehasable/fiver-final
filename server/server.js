03a3b3f90205938c34acfc644f7e69282b2bd1eb
neha sable <nehasable45@gmail.com>
2024-04-09 14:00:00 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -5,16 +5,16 @@ const app=express()
 require('dotenv').config();
 
 const port = process.env.PORT 
-const dbUrl = process.env.DB_URL;
+// const dbUrl = process.env.DB_URL;
 
-mongoose.connect(dbUrl);
+// mongoose.connect(dbUrl);
   
-  const db = mongoose.connection;
+//   const db = mongoose.connection;
  
-  db.on('error', (error) => {
-    console.error('MongoDB connection error:', error);
-  });
-app.use("/api/users",UserRouter)  
+//   db.on('error', (error) => {
+//     console.error('MongoDB connection error:', error);
+//   });
+// app.use("/api/users",UserRouter)  
 
 app.listen(port,()=>{
     console.log("connected")
